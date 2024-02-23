5f8dbf4fafc34436d53a8d136c2fbf258cccce2f
Limber <limbpuma_de@hotmail.com>
2024-02-23 09:50:52 +0100

    Actualizar configuración y resolver problemas de construcción 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel'; // Asegúrate de que esta integración es necesaria para tu despliegue
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-import react from "@astrojs/react";
-
-// https://astro.build/config
+// Configuración de Astro
 export default defineConfig({
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de", "es"],
-    routing:{
+    routing: {
       prefixDefaultLocale: false, // en -> / de -> /de es -> /es
     }
   },
   integrations: [vercel(), tailwind(), react()],
   output: 'server'
-  
 });
