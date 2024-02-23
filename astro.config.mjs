4adfcd3c0305c9b205809b05a7f10a211068e97a
Limber <limbpuma_de@hotmail.com>
2024-02-23 14:40:18 +0100

    Ajustes al componente CookieConsent y otros cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
