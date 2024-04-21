4b82561afd1fdfb0eb3677636a53f9b0946baafb
Limber <limbpuma_de@hotmail.com>
2024-04-21 23:00:23 +0200

    added pwa added rotot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,34 @@
+import fs from 'fs';
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
+// Función para generar el archivo robots.txt
+function generateRobotsTxt() {
+  const content = `User-agent: *
+Disallow:
+
+Sitemap: https://limbermartinez.com/sitemap.xml`;
+  fs.writeFileSync('public/robots.txt', content);
+}
+
+// Función para generar el sitemap.xml (debes completar esta función según la lógica de tu sitio)
+function generateSitemap() {
+  // Tu lógica para generar el sitemap
+  const sitemapContent = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://limbermartinez.com/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <priority>1.00</priority>
+  </url>
+  <!-- Más URLs aquí -->
+</urlset>`;
+  fs.writeFileSync('public/sitemap.xml', sitemapContent);
+}
+
+// Configuración de Astro
 export default defineConfig({
   i18n: {
     defaultLocale: "en",
@@ -15,5 +40,16 @@ export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
   adapter: vercel(),
-  
+  vite: {
+    plugins: [
+      {
+        name: 'custom-build-scripts',
+        enforce: 'build',
+        buildEnd: () => {
+          generateRobotsTxt();
+          generateSitemap();
+        }
+      }
+    ]
+  }
 });
